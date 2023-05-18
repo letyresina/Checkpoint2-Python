44c8c93c98d2e4144d81f4aee696dd7ecdde44dd
letyresina <leticiaresina@gmail.com>
2023-05-18 20:26:45 -0300

    Adicionando validação de opção entre os números solicitados

--- arquivo.py
+++ arquivo.py
@@ -12,15 +12,23 @@ def menuOpcoes():
     print("Digite 3: Visualizar o carrinho de compras;")
     print("Digite 4: Finalizar a compra;")
     print("Digite 5: Finalizar a loja virtual;")
-
+    
     # Validação para saber se o usuário digitou um número inteiro como menu
     try:
         opcao = int(input("Informe a opção desejada: "))
+            # Caso o usuário digite um número, entretanto, não foi entre 1 e 5
+        if (opcao < 1) or (opcao > 5):
+            raise TypeError
         return opcao
     except ValueError:
         print("Por favor, informe somente o número da opção!")
-        print("Aguarde 2 segundos que irá reiniciar automaticamente!")
-        time.sleep(2) # Para dar tempo do usuário ler o que ele fez de errado antes de reiniciar novamente
+        print("Aguarde 1 segundo que irá reiniciar automaticamente!")
+        time.sleep(1) # Para dar tempo do usuário ler o que ele fez de errado antes de reiniciar novamente
+    except TypeError:
+        print("Opção inválida! Por favor, escolha uma opção válida!")
+        print("Aguarde 1 segundo que irá reiniciar automaticamente!")
+        time.sleep(1) # Para dar tempo do usuário ler o que ele fez de errado antes de reiniciar novamente
+
 
 # Mensagem de boas vindas
 print("Seja bem vindo(a) à loja virtual da Vinheria Agnello!")
@@ -28,8 +36,11 @@ print("No que podemos te ajudar hoje?")
 
 while True:
     opcao = menuOpcoes()
-
+    if opcao == 1:
+        print("Implementações futuras...")
+    
     # Encerrando a loja virtual
-    if opcao == 5: 
+    elif opcao == 5: 
         print("Obrigada por utilizar nossa loja virtual! A Vinheria Agnello agradece pela sua escolha e confiança!")
         break
+    
